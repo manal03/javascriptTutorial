048aec5b4579dd673f242bb4e8209a4ebb530bdf
manal03 <118558324+manal03@users.noreply.github.com>
2024-11-11 07:49:11 -0500

    Add files via upload

---
+++ control-assignment-problem/control-assignment-problem/assignment.js
@@ -0,0 +1,14 @@
+// const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+// if(randomNumber > 0.7){
+//     alert('High number')
+// }
+// console.log(randomNumber);
+
+let arrayInput = [1, 45, 2];
+
+for(let arr of arrayInput){
+    console.log(arr);
+}
+for(let i = arrayInput.length; i >=0;i--){
+    console.log(arrayInput[i])
+}
