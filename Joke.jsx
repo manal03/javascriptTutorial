7b10a6eedea5e288289eb4895898d8bc3f85ad8d
manal03 <118558324+manal03@users.noreply.github.com>
2025-01-05 17:57:15 -0500

    React 19
    Updated version of creating the sign up/log in system 
    npm install react@latest react-dom@latest  <-- make sure it is above react 19

--- Joke.jsx
+++ Joke.jsx
@@ -2,55 +2,26 @@ import React from "react"
 import emptyStar from '../assets/globe.png';
 import filledStar from '../assets/marker.png';
 export default function Joke() {
-    const [contact, setContact] = React.useState({
-        firstName: "John",
-        lastName: "Doe",
-        phone: "+1 (212) 555-1212",
-        email: "itsmyrealname@example.com",
-        isFavorite: true
-    })
-    /**
-     * Challenge: Fill in the values in the markup
-     * using the properties of our state object above
-     * (Ignore `isFavorite` for now)
-     */
-    function toggleFavorite() {
-        setContact(
-            prevContact=>{
-                return{
-                    ...prevContact,
-                    isFavorite : !contact.isFavorite
+     function refreshClick(formData){
+        const email = formData.get("email")
+        const password = formData.get("password")
+        console.log(email)
 
-                }
-            }
-            
-        )
     }
-    let starIcon = contact.isFavorite ? filledStar : emptyStar
     return (
-        <main>
-            <article className="card">
-                <div className="info">
-                    <button
-                        onClick={toggleFavorite}
-                        aria-pressed={false}
-                        className="favorite-button"
-                    >   
-                    <img src={starIcon}
-                    className='favourite'
-                    alt = {contact.isFavorite ? 'Filled Star Icon' : 'Empty star'} 
-                    aria-label = {contact.isFavorite ? 'True' : 'False'}
-                    aria-pressed = {contact.isFavorite ? 'Remove from favourites' : 'Add to favourites'}
-                    />
-                    </button>
-                    <h2 className="name">
-                        {contact.firstName} {contact.lastName}
-                    </h2>
-                    <p className="contact">{contact.phone}</p>
-                    <p className="contact">{contact.email}</p>
-                </div>
-
-            </article>
-        </main>
+        <section>
+            <h1>Sign Up Form</h1>
+            <form action={refreshClick}>
+                <label htmlFor="email">Email:
+                <input id="email" type="text" name="email" placeholder="joe@schmoe.com"/>
+                </label>
+                <br />
+                <label htmlFor="password"> Password: 
+                <input type="password" name="password" placeholder="password12"/>
+                </label>
+                <br />
+                <input type="submit" value="Click"/>
+            </form>
+        </section>
     )
 }
