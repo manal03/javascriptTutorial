6c2b6c27712444cd364cd770d61b1ef0c320aafa
manal03 <118558324+manal03@users.noreply.github.com>
2025-01-05 19:05:04 -0500

    forms
    Used before learning about maps and keys

--- Joke.jsx
+++ Joke.jsx
@@ -6,12 +6,20 @@ export default function Joke() {
         const email = formData.get("email")
         const password = formData.get("password")
         console.log(email)
+        console.log(password)
+        const textArea = formData.get("description")
+        const employmentStatus = formData.get("employmentStatus")
+        const dietaryRestrictions = formData.getAll("dietaryRestrictions")
+
+        console.log(textArea)
+        console.log(employmentStatus)
+        console.log(dietaryRestrictions)
 
     }
     return (
         <section>
             <h1>Sign Up Form</h1>
-            <form action={refreshClick}>
+            <form className="thing" action={refreshClick}>
                 <label htmlFor="email">Email:
                 <input id="email" type="text" name="email" placeholder="joe@schmoe.com"/>
                 </label>
@@ -21,6 +29,43 @@ export default function Joke() {
                 </label>
                 <br />
                 <input type="submit" value="Click"/>
+                <label htmlFor="description">Description:</label>
+                <textarea id="description" name="description" defaultValue={"this is a empty desciprtion"}></textarea>
+
+                <fieldset>
+                    <legend>Employment Status</legend>
+                <label>
+                <input type="radio" name="employmentStatus" value="unemployed"/>
+                Unemployed
+                </label>
+                <label>
+                <input type="radio" name="employmentStatus" value="part-time"/>
+                Part time
+                </label>
+                <label>
+                <input type="radio" name="employmentStatus" value="full-time"/>
+                Full time
+                </label>
+                </fieldset>
+
+
+                <fieldset>
+                    <legend>Dietary Restrictions</legend>
+                <label>
+                <input type="checkbox" name="dietaryRestrictions" value="kosher"/>
+                Kosher
+                </label>
+                <label>
+                <input type="checkbox" name="dietaryRestrictions" value="vegan"/>
+                Vegan
+                </label>
+                <label>
+                <input type="checkbox" name="dietaryRestrictions" value="gluten-free"/>
+                Gluten Free
+                </label>
+                </fieldset>
+
+                
             </form>
         </section>
     )
